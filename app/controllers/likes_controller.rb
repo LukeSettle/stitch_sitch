ffbff0ef45c7e27320fe77c5cc04686710f463be
LukeSettle <luke.settle@metova.com>
2016-04-22 02:59:26 -0500

    only one like per picture per user

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,6 @@
 class LikesController < ApplicationController
   def create
-    # unless current_user.likes.find_by(picture_id: params[:picture_id])
+    unless current_user.likes.find_by(picture_id: params[:picture_id])
       @like = Like.new(picture_id: params[:picture_id], user_id: current_user.id)
       @picture = Picture.find(params[:picture_id])
       if @like.save
@@ -8,6 +8,6 @@ class LikesController < ApplicationController
       else
         render json: @like.errors.full_messages
       end
-    # end
+    end
   end
 end
